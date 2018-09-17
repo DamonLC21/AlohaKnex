a8e1ebf953c820afc89d2e9dec54560bb7b178f0
Damon Chivers <chiversdamon@gmail.com>
2018-09-17 09:00:55 -0600

    all queries

--- app.js
+++ app.js
@@ -8,27 +8,35 @@ const queries = require('./queries.js')
 app.use(cors())
 app.use(bodyParser.json())
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     queries.getAll()
-    .then(response => res.send(response))
+        .then(response => res.send(response))
 })
 
 
-app.get('/:id', (req,res) =>{
+app.get('/:id', (req, res) => {
     queries.getById(req.params.id)
-    .then(response => res.send(response))
+        .then(response => res.send(response))
 })
 
-app.post('/',(req,res) => {
+app.post('/', (req, res) => {
     res.send('🍌')
     queries.create(req.body)
-    .then(response => res.json({response}))
+        .then(response => res.json({
+            response
+        }))
 })
 
+app.put('/:id', (req, res) => {
+    queries.updateStudent(req.params.id, req.body)
+        .then(data => res.json(data))
+})
 
-
-
+app.delete('/:id', (req, res) => {
+    queries.delete(req.params.id)
+        .then(res.status(204))
+})
--- queries.js
+++ queries.js
@@ -2,15 +2,25 @@ const database = require('./knexfile.js')[process.env.NODE_ENV || 'development']
 const knex = require('knex')(database)
 
 module.exports = {
-    getAll(){
-        return knex.select().from('g95')
+    getAll() {
+        return knex.select().from('g95').orderBy('id', 'ascend')
     },
-    getById(id){
+    getById(id) {
         return knex('g95')
-          .where('id', id)
-       },
-    create(newPost){
+            .where('id', id)
+    },
+    create(newPost) {
         return knex('g95')
-        .insert(newPost)
+            .insert(newPost)
+    },
+    updateStudent(id, student) {
+        return knex('g95')
+            .where('id', id)
+            .update(student)
+    },
+    delete(id) {
+        return knex('g95')
+            .where('id', id)
+            .delete()
     }
 }
